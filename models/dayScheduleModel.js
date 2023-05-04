e326716dcf7b8ddb35d36c811d298f6ba46029eb
Nikko-Adrian-Pacleb <pacleb.nikko@gmail.com>
2023-05-04 02:18:27 -0700

    Model Updates

--- models/dayScheduleModel.js
+++ models/dayScheduleModel.js
@@ -2,17 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Company = require("./companyModel");
 const Employee = require("./employeeModel");
+const debug = require("debug")("app:dayScheduleModel");
 
 // Define collection and schema for Schedule
 const DaySchedule = new Schema(
   {
     // Schedule Company Information
-    ScheduleCompany: {
+    DayScheduleCompany: {
       type: Schema.Types.ObjectId,
       ref: "Company",
       required: true,
     },
-    ScheduleDate: { type: Date, required: true },
+    DayScheduleDate: { type: Date, required: true },
     ScheduleInformation: [
       {
         ScheduleEmployee: {
@@ -24,13 +25,6 @@ const DaySchedule = new Schema(
         ScheduleEndTime: { type: Date, required: true },
       },
     ],
-
-    // Expire in 30 days
-    expireAt: {
-      type: Date,
-      default: ScheduleDate,
-      index: { expires: "30d" },
-    },
   },
   {
     timestamps: true,
--- models/employeeTimeRequestModel.js
+++ models/employeeTimeRequestModel.js
@@ -2,30 +2,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Company = require("./companyModel");
 const Employee = require("./employeeModel");
+const { bulkSave } = require("./dayScheduleModel");
+const debug = require("debug")("app:employeeTimeRequestModel");
 
 // Define collection and schema for Employee
 const EmployeeTimeRequest = new Schema(
   {
     // Employee Company Information
-    EmployeeCompany: {
+    EmployeeTimeRequestCompany: {
       type: Schema.Types.ObjectId,
       ref: "Company",
       required: true,
     },
-    Employee: { type: Schema.Types.ObjectId, ref: "Employee", required: true },
+    EmployeeTimeRequestEmployee: {
+      type: Schema.Types.ObjectId,
+      ref: "Employee",
+      required: true,
+    },
 
     // Request Information
     EmployeeTimeRequestDate: { type: Date, required: true },
     EmployeeTimeRequestAllDay: { type: Boolean, required: true },
     EmployeeTimeRequestStartTime: { type: Date, required: true },
     EmployeeTimeRequestEndTime: { type: Date, required: true },
-
-    // Expire in 30 days
-    expireAt: {
-      type: Date,
-      default: EmployeeTimeRequestDate,
-      index: { expires: "30d" },
-    },
   },
   {
     timestamps: true,
