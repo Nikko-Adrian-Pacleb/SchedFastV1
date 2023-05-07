786e36474bcb3e1ce09ea0eccafdf41a83febcbd
Nikko-Adrian-Pacleb <pacleb.nikko@gmail.com>
2023-05-07 01:58:14 -0700

    Initial Company Controller

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,3 +1,36 @@
 exports.company_index_get = (req, res) => {
   res.render("company_index", { title: "Company Index" });
 };
+
+exports.company_detail_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Company Detail GET");
+};
+
+exports.company_employees_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Company Employees GET");
+};
+
+exports.company_employee_detail_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Company Employee Detail GET");
+};
+
+exports.company_employee_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Company Employee Create GET");
+};
+exports.company_employee_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Company Employee Create POST");
+};
+
+exports.company_employee_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Company Employee Update GET");
+};
+exports.company_employee_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Company Employee Update POST");
+};
+
+exports.company_employee_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Company Employee Delete GET");
+};
+exports.company_employee_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Company Employee Delete POST");
+};
