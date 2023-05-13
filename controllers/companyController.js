dfc74a65ba0481561c9da7d70d062d1a472be368
Nikko-Adrian-Pacleb <pacleb.nikko@gmail.com>
2023-05-12 22:46:44 -0700

    FindByID not working

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -3,6 +3,7 @@ const asynchandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
 const { body, validationResult } = require("express-validator");
 const Company = require("../models/companyModel");
+const Employee = require("../models/employeeModel");
 
 exports.company_index_get = (req, res) => {
   res.render("company_index", { title: "Company Index" });
@@ -82,12 +83,86 @@ exports.company_employee_detail_get = (req, res) => {
 exports.company_employee_create_get = (req, res) => {
   res.render("company_employee_create", {
     title: "Create Employee",
+    errors: false,
     company: req.user,
   });
 };
-exports.company_employee_create_post = (req, res) => {
-  res.send(req.body);
-};
+exports.company_employee_create_post = [
+  body("EmployeeCompany", "Employee Company ID required")
+    .trim()
+    .isLength({ min: 1 }),
+  body("EmployeePosition", "Employee Position required")
+    .trim()
+    .isLength({ min: 1 }),
+  body("EmployeeCompanyID", "Employee Company Code required")
+    .trim()
+    .isLength({ min: 1 }),
+  body("EmployeeID", "Employee ID required").trim().isLength({ min: 1 }),
+  body("EmployeePin", "Employee Pin required").trim().isLength({ min: 1 }),
+  body("EmployeeFirstName", "Employee First Name required")
+    .trim()
+    .isLength({ min: 1 }),
+  body("EmployeeLastName", "Employee Last Name required")
+    .trim()
+    .isLength({ min: 1 }),
+  asynchandler(async (req, res, next) => {
+    const {
+      EmployeeCompany,
+      EmployeeCompanyID,
+      EmployeePosition,
+      EmployeeID,
+      EmployeePin,
+      EmployeeFirstName,
+      EmployeeLastName,
+    } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("company_employee_create", {
+        title: "Create Employee",
+        company: req.user,
+        errors: errors.array(),
+      });
+      return;
+    }
+    console.log(EmployeeCompanyID);
+    const company = await Company.findById(EmployeeCompany);
+    if (!company) {
+      res.render("company_employee_create", {
+        title: "Create Employee",
+        company: req.user,
+        errors: [{ msg: "Company does not exist" }],
+      });
+      return;
+    }
+
+    console.log(company);
+    console.log(company.CompanyEmployees);
+    for (let i = 0; i < company.CompanyEmployees.length; i++) {
+      const employee = await Employee.findById(company.CompanyEmployees[i]);
+      if (employee.EmployeeID == EmployeeID) {
+        res.render("company_employee_create", {
+          title: "Create Employee",
+          company: req.user,
+          errors: [{ msg: "Employee already exists" }],
+        });
+        return;
+      }
+    }
+
+    const newEmployee = new Employee({
+      EmployeeCompany: EmployeeCompany,
+      EmployeePosition: EmployeePosition,
+      EmployeeCompanyID: EmployeeCompanyID,
+      EmployeeID: EmployeeID,
+      EmployeePin: EmployeePin,
+      EmployeeFirstName: EmployeeFirstName,
+      EmployeeLastName: EmployeeLastName,
+    });
+    await newEmployee.save();
+    res.redirect("/company/employees");
+  }),
+];
 
 exports.company_employee_update_get = (req, res) => {
   res.send("NOT IMPLEMENTED: Company Employee Update GET");
